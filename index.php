0829e1f9ded81963e54f39dc1fcb3b176022ada9
Alethepata <alessia.patane@gmail.com>
2023-11-07 15:45:45 +0100

    [Add] setYearReleased

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
 class Movie{
     public $name;
+    public $discount = 0;
+    private $yearReleased = 0;
 
     function __construct($_name){
         $this->name = $_name;
     }
+    
+    public function setYearReleased($_years){
+        $this->yearReleased = $_years;
+        if($_years < 2000){
+            $this->discount = 60;
+        }else{
+            $this->discount = 10;
+        }
+    }
 }
 
 $barbie = new Movie('Barbie');
+$barbie->setYearReleased(2023);
+
 $batman = new Movie('Batman');
+$batman->setYearReleased(1989);
 
 var_dump($barbie);
 var_dump($batman);
