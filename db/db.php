b500e7a924040d3fc38393227a1d4205a7454bd3
Alethepata <alessia.patane@gmail.com>
2023-11-08 14:14:34 +0100

    [Add] productions

---
+++ db/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$productions = [
+    new Movie('Barbie', 2023, '1h 54m'),
+    new Movie('Batman', 1989, '2h 6m'),
+    new Movie('Il gigante di ferro', 1999, '1h 26m'),
+    new Movie('Ralph Spaccatutto', 2012, '1h 41m'),
+    new SerieTv('Once Upon a Time', 2011, 7, 156, 2018),
+
+];
