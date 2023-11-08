0a8bd73a0651a469e3d75ece1a716d7274bcb905
Alethepata <alessia.patane@gmail.com>
2023-11-08 14:21:35 +0100

    [Add] SerieTv

--- db/db.php
+++ db/db.php
@@ -6,5 +6,8 @@ $productions = [
     new Movie('Il gigante di ferro', 1999, '1h 26m'),
     new Movie('Ralph Spaccatutto', 2012, '1h 41m'),
     new SerieTv('Once Upon a Time', 2011, 7, 156, 2018),
+    new SerieTv('Caleidoscopio', 2023, 1, 8, 2023),
+    new SerieTv('Hannah Montana', 2006, 4, 98, 2011),
+    new SerieTv('Stranger Things', 2016, 4, 34, 2022)
 
 ];
