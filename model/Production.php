fa415e69fd4b32596cad9d967c4dfbf284b55aea
Alethepata <alessia.patane@gmail.com>
2023-11-08 14:10:59 +0100

    [Add] Production

---
+++ model/Production.php
@@ -0,0 +1,14 @@
+<?php
+
+class Production{
+    public $name;
+    public $yearReleased;
+
+    public function __construct($_name, $_year){
+
+        $this->name = $_name;
+        $this->yearReleased = $_year;
+        
+    }
+    
+}
