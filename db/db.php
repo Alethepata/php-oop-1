5ec00579729a9cb0500cea9990c2f3ee9c154538
Alethepata <alessia.patane@gmail.com>
2023-11-08 17:46:45 +0100

    [Edit] productions -> name

--- db/db.php
+++ db/db.php
@@ -2,7 +2,7 @@
 
 $productions = [
     
-    new Movie('Barbie', 21, 7, 2023, '1h 54m'),
+    new Movie('', 21, 7, 2023, '1h 54m'),
     new Movie('Batman', 20, 10, 1989, '2h 6m'),
     new Movie('Il gigante di ferro', 23, 12, 1999, '1h 26m'),
     new Movie('Ralph Spaccatutto', 20, 12, 2012, '1h 41m'),
